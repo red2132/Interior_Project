0fb61b0fdbee0ea277238634a7c3d552dd0b202e
LG <LG@LG-PC>
2021-11-02 09:23:46 +0900

    QnA관련 업데이트

--- springProject/src/main/java/com/sist/dao/QandADAO.java
+++ springProject/src/main/java/com/sist/dao/QandADAO.java
@@ -10,5 +10,17 @@ public class QandADAO {
 	@Autowired
 	private QandAMapper mapper;
 	
+	//1. Q&A 게시글 리스트
+	
+	//2. Q&A 게시글 작성
+	
+	//3. Q&A 게시글/답변게시글 상세보기
+	
+	//4. Q&A 답변게시글 작성
+	
+	
+	//5. Q&A 게시글/답변게시글 수정
+	
+	//6. Q&A 게시글/답변게시글 삭제
 	
 }
--- springProject/src/main/java/com/sist/web/QandAController.java
+++ springProject/src/main/java/com/sist/web/QandAController.java
@@ -2,6 +2,8 @@ package com.sist.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 import java.util.*;
 import com.sist.dao.*;
 import com.sist.vo.*;
@@ -10,6 +12,40 @@ import com.sist.vo.*;
 public class QandAController {
 	@Autowired
 	private QandADAO dao;
+	
+	//1. QandA 게시글 목록 출력
+	public String qnaList(String page,Model model) {
+		//1-1. 페이지 설정
+		if(page==null)
+			page="1";
+		int curpage=Integer.parseInt(page);
+		int rowSize=5;
+		int end=(rowSize*curpage);
+		int start=end-rowSize+1;
+		Map map=new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+//		List<QandAVO> qnaList=dao.list~
+		
+		//1-2. 페이지나누기
+//		int totalpage=dao.totalpage();
+		final int BLOCK=5;
+		int startPage=((curpage-1)/BLOCK*BLOCK)+1;
+		int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+//		if(endPage>totalpage)
+//			endPage=totalpage;
+	
+		//1-3. MODEL이용하여 JSP에서 출력할 값 전송
+		model.addAttribute("curpage", curpage);
+//		model.addAttribute("totalpage", totalpage);
+		model.addAttribute("BLOCK", BLOCK);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+//		model.addAttribute("qnaList", qnaList);
+		model.addAttribute("main_jsp", "../qna/list.jsp");
+		return "main/main";
+	}
+	
 	
 	
 }
