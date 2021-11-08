2bfc82718b78448e5133c08e28c4a60e022fd486
박성경 <@210.178.236.164>
2021-11-08 15:05:44 +0900

    cart,mypage VO추가

---
+++ springProject/src/main/java/com/sist/vo/MypageVO.java
@@ -0,0 +1,12 @@
+package com.sist.vo;
+/*
+ *     id VARCHAR2(30),
+	   buy_no NUMBER,
+	   sell_no NUMBER,
+	   interesting_no CLOB,
+	   interesting_cate CLOB
+ */ 
+public class MypageVO {
+	private String id, interesting_no, interesting_cate;
+	private int buy_no,sell_no;
+}
